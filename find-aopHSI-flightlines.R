3dd5a935b81ff118b3780c173624710aea34a18e
Naupaka Zimmerman <naupaka@gmail.com>
2016-06-24 11:46:10 -0600

    Add recursive flag to directory creation call

--- find-aopHSI-flightlines.R
+++ find-aopHSI-flightlines.R
@@ -134,7 +134,9 @@ write_shapefile_bound <- function(f, shpDir, proj4Str){
   
   # create the output directory we want to use if it doesn't yet exist
   ifelse(!dir.exists(file.path(shpDir)), 
-	   dir.create(file.path(shpDir), showWarnings = TRUE))
+         dir.create(file.path(shpDir),
+                    showWarnings = TRUE,
+                    recursive = TRUE))
   
   writeOGR(sp.obj, 
            shpDir, #path to export to
